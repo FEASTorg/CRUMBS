470d622b4a60d53943be611c25223126cbf9e1be
Cameron K Brooks <cambrooks3393@gmail.com>
2025-11-23 23:45:45 -0500

    add shim file

---
+++ src/CRUMBS.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "crumbs/crumbs.h"
+#include "crumbs/crumbs_i2c.h"
+#include "crumbs/crumbs_crc.h"
+#include "crumbs/crumbs_message.h"
+#ifdef ARDUINO
+#include "crumbs/crumbs_arduino.h"
+#endif
